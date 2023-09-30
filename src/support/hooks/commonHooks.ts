15d4e25a085a2ca1c29c59936e712ace68bca0ff
xn0-MM <chanomm75@gmail.com>
2023-09-30 13:30:07 +0200

    feat: se añaden los hooks y el world

---
+++ src/support/hooks/commonHooks.ts
@@ -0,0 +1,32 @@
+
+import { After, AfterAll, Before, BeforeAll, setDefaultTimeout, Status } from "@cucumber/cucumber"
+import { Browser} from "playwright"
+import { ICustomWorld } from "../worlds/world"
+import { Pom } from "../../pages/pom"
+import config from '../../../config';
+  
+  setDefaultTimeout(config.defaultTimeout)
+  
+  let browser: Browser;
+  
+  BeforeAll(async function () {
+  })
+  
+  Before(async function (this: ICustomWorld) {
+    this.context = await browser.newContext({
+      viewport: { width: 1920, height: 1080 },
+    });
+
+    this.page = await this.context.newPage();
+    this.pom = new Pom(this.page, this.context)
+  });
+  
+  After(async function (this: ICustomWorld , {pickle, result}) {
+
+    await this.context?.close()
+    await this.page?.close()
+  });
+  
+  AfterAll(async function () {
+    await browser.close()
+  });
---
+++ src/support/worlds/world.ts
@@ -0,0 +1,21 @@
+import { setWorldConstructor, World, IWorldOptions } from '@cucumber/cucumber';
+import { BrowserContext, Page, PlaywrightTestOptions } from '@playwright/test';
+import { Pom } from '../../pages/pom';
+
+
+
+export interface ICustomWorld extends World {
+  context?: BrowserContext
+  page?: Page
+  pom?: Pom
+
+  playwrightOptions?: PlaywrightTestOptions;
+}
+
+export class CustomWorld extends World implements ICustomWorld {
+  constructor(options: IWorldOptions) {
+    super(options)
+  }
+}
+
+setWorldConstructor(CustomWorld);
