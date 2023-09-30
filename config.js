db578b08ac475c5fdf230ffc4587daba52fd1cd0
xn0-MM <chanomm75@gmail.com>
2023-10-01 00:17:55 +0200

    refactor: modificaciones para poder introducir variables en el archivo cucumber.js desde el archivo de configuración

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  baseUrl: process.env.BASE_URL,
+  browser: process.env.BROWSER || 'chrome', 
+  defaultTimeout: 60 * 1000, // milliseconds
+  headless: true,
+  runSlow: 0, 
+  pwTimeout: 40 * 1000,
+  screenshotsIfFail: false,
+  worker: 3,
+  screenshotByStep: true
+}
+  
+module.exports = config;
+  
--- cucumber.js
+++ cucumber.js
@@ -1,15 +1,20 @@
+const config = require('./config')
+
+
 
 const cucumberConfig = {
   default: `
     --require-module ts-node/register
     --require src/**/*.steps.ts
     --require src/**/*.ts
+    
     --format progress-bar 
     --format json:reports/cucumber-report.json
     --format @cucumber/pretty-formatter
     --format-options ${JSON.stringify({ snippetInterface: 'async-await' })}
-    --parallel 1
+    --parallel ${config.worker}
     src/test/features/*.feature
+    config.ts
   `
 }
 
