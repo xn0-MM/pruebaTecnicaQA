947c16219de0ad9b8528b76c3d5bdb3ef257b428
xn0-MM <chanomm75@gmail.com>
2023-10-01 15:23:37 +0200

    feat: se añade nueva feature y su respectivo archivo step asi como un nuevo comando para ejecutar por el tag contadorTarea

---
+++ src/test/steps/contadorTareas.step.ts
@@ -0,0 +1,12 @@
+import { Then } from '@cucumber/cucumber'
+import { ICustomWorld } from '../../support/worlds/world'
+import { expect } from 'chai'
+import { captureScreenshotByStep} from '../../support/helpers/utils/utils'
+
+
+
+Then('el contador debería mostrar {int} items left', async function (this: ICustomWorld, numTareasPendientes: number) {
+    const number = Number(await this.pom?.homePage.counter.innerText())
+    expect(number).to.eql(numTareasPendientes)
+    await captureScreenshotByStep(this.page, this.attach)
+})
