444bc1cc43063782ee212ae0dda6775781b4bf43
xn0-MM <chanomm75@gmail.com>
2023-10-01 02:25:12 +0200

    ci: fix para poder ejecutar los tests en ci, se ha añadido una nueva variable que permite activar o desactivar la paralelización

--- cucumber.js
+++ cucumber.js
@@ -1,18 +1,29 @@
 const config = require('./config')
 
-const cucumberConfig = {
-  default: `
-    --require-module ts-node/register
-    --require src/**/*.steps.ts
-    --require src/**/*.ts
-    
-    --format progress-bar 
-    --format json:reports/cucumber-report.json
-    --format @cucumber/pretty-formatter
-    --format-options ${JSON.stringify({ snippetInterface: 'async-await' })}
-    --parallel ${config.worker}
-    src/test/features/*.feature
-  `
+  function getParallelOption(isParallel, config) {
+    if (isParallel) {
+        return `--parallel ${config.worker}`;
+    }
+    return '';
+  }
+
+  const parallelOption = getParallelOption(config.parallel, config.worker)
+  const cucumberConfig = {
+    default: `
+      --require-module ts-node/register
+      --require src/**/*.steps.ts
+      --require src/**/*.ts
+      
+      --format progress-bar 
+      --format json:reports/cucumber-report.json
+      --format @cucumber/pretty-formatter
+      --format-options ${JSON.stringify({ snippetInterface: 'async-await' })}
+      ${parallelOption}
+      src/test/features/*.feature
+    `
+  }
+
 }
+
 
 module.exports = cucumberConfig
--- src/support/helpers/env/env.ts
+++ src/support/helpers/env/env.ts
@@ -3,10 +3,7 @@ import * as dotenv from 'dotenv'
 export const getEnv = () => {
     if (process.env.ENV) {
         if(process.env)
-        dotenv.config({
-            override: true,
-            path: `./.env`
-        })
+            dotenv.config()
     } else {
         console.error("No hay un .env!")
     }
