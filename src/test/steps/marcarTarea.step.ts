ba13098a3bd2b4d4bd33b02c9b3d6fdc9c611f12
xn0-MM <chanomm75@gmail.com>
2023-10-01 15:17:59 +0200

    feat: se añade un nuevo scenario outline para marcarTareas y se añade un nuevo step

--- src/test/steps/marcarTarea.step.ts
+++ src/test/steps/marcarTarea.step.ts
@@ -1,22 +1,22 @@
-import { Then } from '@cucumber/cucumber'
+import { When, Then } from '@cucumber/cucumber'
 import { ICustomWorld } from '../../support/worlds/world'
 import { expect } from 'chai'
 import { captureScreenshotByStep} from '../../support/helpers/utils/utils'
 
+   When('hace click en el botón Mark all as completed', async function (this: ICustomWorld) {
+      await this.pom?.homePage.markAllCompletedButton.click()
+      await captureScreenshotByStep(this.page, this.attach)
+   });
 
+   Then('deberían aparecer las tareas {string} marcadas', async function (this: ICustomWorld, tareas: string) {
+      const tareasCompletadas = await this.pom?.homePage.getArrayTareasCompletadas()
+      const arrayTareasCompletadas: string[] = tareas.split(",").map(tarea => tarea.trim())
+      
+      expect(tareasCompletadas).to.have.members(arrayTareasCompletadas)
+      await captureScreenshotByStep(this.page, this.attach)
+   });
 
-
-         Then('deberían aparecer las tareas {string} marcadas', async function (this: ICustomWorld, tareas: string) {
-            const tareasCompletadas = await this.pom?.homePage.getArrayTareasCompletadas()
-            const arrayTareasCompletadas: string[] = tareas.split(",").map(tarea => tarea.trim())
-            expect(tareasCompletadas).to.eql(arrayTareasCompletadas)
-            await captureScreenshotByStep(this.page, this.attach)
-         });
-
-
-
-         Then('deberían aparecer {int} tareas marcadas', async function (this: ICustomWorld, numExpectTareas: number) {
-            expect(await this.pom?.homePage.getNumberOfTareasCompletadas()).to.eql(numExpectTareas)
-            await captureScreenshotByStep(this.page, this.attach)
-         });
-
+   Then('deberían aparecer {int} tareas marcadas', async function (this: ICustomWorld, numExpectTareas: number) {
+      expect(await this.pom?.homePage.getNumberOfTareasCompletadas()).to.eql(numExpectTareas)
+      await captureScreenshotByStep(this.page, this.attach)
+   });
