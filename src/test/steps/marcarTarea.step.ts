64e0919912aa56ef7e4acffed4eb906e4854e487
xn0-MM <chanomm75@gmail.com>
2023-10-01 06:24:22 +0200

    refactor: se eliminan comentarios residuales en el step de marcarTareas

--- src/test/steps/marcarTarea.step.ts
+++ src/test/steps/marcarTarea.step.ts
@@ -9,8 +9,6 @@ import { captureScreenshotByStep} from '../../support/helpers/utils/utils'
          Then('deberían aparecer las tareas {string} marcadas', async function (this: ICustomWorld, tareas: string) {
             const tareasCompletadas = await this.pom?.homePage.getArrayTareasCompletadas()
             const arrayTareasCompletadas: string[] = tareas.split(",").map(tarea => tarea.trim())
-            console.log(tareasCompletadas)
-            console.log(arrayTareasCompletadas)
             expect(tareasCompletadas).to.eql(arrayTareasCompletadas)
             await captureScreenshotByStep(this.page, this.attach)
          });
@@ -19,6 +17,6 @@ import { captureScreenshotByStep} from '../../support/helpers/utils/utils'
 
          Then('deberían aparecer {int} tareas marcadas', async function (this: ICustomWorld, numExpectTareas: number) {
             expect(await this.pom?.homePage.getNumberOfTareasCompletadas()).to.eql(numExpectTareas)
-            await captureScreenshotByStep(this.page, this.attach) // Then('deberían aparecer {float} tareas marcadas', async function (float) {
+            await captureScreenshotByStep(this.page, this.attach)
          });
 
